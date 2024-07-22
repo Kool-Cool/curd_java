6e8a66e758eeaa18dfad1f9ceb78ae723773e791
Kool-Cool <112835534+Kool-Cool@users.noreply.github.com>
2024-07-22 07:16:35 +0530

    initial Commit

---
+++ src/main/java/com/example/curd_java/CurdJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.curd_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdJavaApplication.class, args);
+	}
+
+}
